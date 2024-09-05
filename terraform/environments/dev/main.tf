5ad6c3f00ae2ca88b65d691ba2f88a23feb917a8
rjraju143 <bangarraju1152@gmail.com>
2024-09-05 22:49:50 +0530

    added dev-user module

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"  # or your preferred region
+  region = "ap-south-1" # or your preferred region
 }
 
 terraform {
@@ -7,8 +7,8 @@ terraform {
 
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.49"
+      source  = "hashicorp/aws"
+      version = "~> 5.49"
     }
   }
 }
@@ -16,23 +16,30 @@ terraform {
 module "vpc" {
   source = "../../modules/0-vpc"
 
-  cidr_block        = "10.0.0.0/16"
-  name              = "dev-main"
+  cidr_block          = "10.0.0.0/16"
+  name                = "dev-main"
   public_subnet1_cidr = "10.0.64.0/19"
   public_subnet2_cidr = "10.0.96.0/19"
-  zone1             = "ap-south-1a"
-  zone2             = "ap-south-1b"
-  cluster_name      = "dev-cluster"
+  zone1               = "ap-south-1a"
+  zone2               = "ap-south-1b"
+  cluster_name        = "dev-cluster"
 }
 
 module "eks" {
   source = "../../modules/1-eks"
 
   name           = "dev-cluster"
-  eksversion     = "1.30"  # or your preferred EKS version
+  eksversion     = "1.30" # or your preferred EKS version
   subnet_ids     = [module.vpc.public_subnet1_id, module.vpc.public_subnet2_id]
   instance_types = ["t2.small"]
   desired_size   = 1
   max_size       = 3
   min_size       = 1
+}
+
+module "dev_user" {
+  source            = "../../modules/2-dev-user"
+  user_name         = "developer"
+  cluster_name      = "dev-cluster"
+  kubernetes_groups = ["dev-user"]
 }
