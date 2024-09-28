63742c4e10d39437b45550496ebc389d5cd12a3f
rjraju143 <bangarraju1152@gmail.com>
2024-09-28 23:13:24 +0530

    efs driver

--- modules/efs-csi-driver/variables.tf
+++ modules/efs-csi-driver/variables.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:modules/efs-csi-driver/variables.tf
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
@@ -24,30 +23,3 @@ variable "cluster_security_group_id" {
   type        = list(string)
 }
 
-=======
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
-}
- 
-variable "cluster_oidc_issuer_url" {
-  description = "The URL on the EKS cluster for the OpenID Connect identity provider"
-  type        = string
-}
- 
-variable "subnet_id_1" {
-  description = "ID of the first subnet to create an EFS mount target"
-  type        = string
-}
- 
-variable "subnet_id_2" {
-  description = "ID of the second subnet to create an EFS mount target"
-  type        = string
-}
- 
-variable "cluster_security_group_id" {
-  description = "Security group ID of the EKS cluster"
-  type        = string
-}
-
->>>>>>> parent of 2541bc5 (efs module):terraform/modules/efs-csi-driver/variables.tf
--- terraform/modules/efs-csi-driver/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "eks_cluster_name" {
-  value = aws_eks_cluster.eks.name
-}
-
-output "eks_oidc_identity" {
-  value = aws_eks_cluster.eks.identity[0].oidc[0].issuer
-}
-
