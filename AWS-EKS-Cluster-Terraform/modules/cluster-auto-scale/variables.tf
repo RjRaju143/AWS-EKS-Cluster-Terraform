2050229b9a6c579cb3d99139cc905f2d60bc4398
RjRaju143 <bangarraju1152@gmail.com>
2024-10-26 23:05:12 +0530

    cluster-auto-scale

---
+++ AWS-EKS-Cluster-Terraform/modules/cluster-auto-scale/variables.tf
@@ -0,0 +1,9 @@
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
+
+variable "region" {
+  description = "awsRegion cluster_autoscaler"
+  type        = string
+}
