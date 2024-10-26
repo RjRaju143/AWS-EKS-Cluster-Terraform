896fd19fe1ae5414cc73a6df3547c2aae8d1a10d
RjRaju143 <bangarraju1152@gmail.com>
2024-10-26 23:06:42 +0530

    openid_connect_provider

---
+++ AWS-EKS-Cluster-Terraform/modules/openid_connect_provider/main.tf
@@ -0,0 +1,10 @@
+data "tls_certificate" "eks" {
+  url = var.url
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = var.url
+}
+
