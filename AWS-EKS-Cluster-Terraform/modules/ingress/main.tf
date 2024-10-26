f127315d5f21296d2f2cbc09f50430fb29442378
RjRaju143 <bangarraju1152@gmail.com>
2024-10-26 23:06:14 +0530

    ingress

---
+++ AWS-EKS-Cluster-Terraform/modules/ingress/main.tf
@@ -0,0 +1,78 @@
+### NLB Ingress
+resource "aws_iam_role" "aws_lbc" {
+  name               = "${var.cluster_name}-aws-lbc"
+  assume_role_policy = data.aws_iam_policy_document.aws_lbc.json
+}
+
+resource "aws_iam_policy" "aws_lbc" {
+  policy = file("${path.module}/iam/AWSLoadBalancerController.json")
+  name   = "AWSLoadBalancerController"
+}
+
+resource "aws_iam_role_policy_attachment" "aws_lbc" {
+  policy_arn = aws_iam_policy.aws_lbc.arn
+  role       = aws_iam_role.aws_lbc.name
+}
+
+resource "aws_eks_pod_identity_association" "aws_lbc" {
+  cluster_name    = var.cluster_name
+  namespace       = "kube-system"
+  service_account = "aws-load-balancer-controller"
+  role_arn        = aws_iam_role.aws_lbc.arn
+}
+
+resource "helm_release" "aws_lbc" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.8.1"
+
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "vpcId"
+    value = var.vpc_id
+  }
+
+  # depends_on = [helm_release.cluster_autoscaler]
+}
+
+resource "helm_release" "external_nginx" {
+  name             = "external"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress"
+  create_namespace = true
+  version          = "4.10.1"
+  values           = [file("${path.module}/values/nginx-ingress.yaml")]
+
+  depends_on = [helm_release.aws_lbc]
+}
+
+### Cert Manager
+resource "helm_release" "cert_manager" {
+  name = "cert-manager"
+
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+  version          = "v1.15.0"
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+
+  depends_on = [helm_release.external_nginx]
+}
+
---
+++ AWS-EKS-Cluster-Terraform/modules/ingress/variables.tf
@@ -0,0 +1,9 @@
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
